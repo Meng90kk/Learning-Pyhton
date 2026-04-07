7710d2ad68efdfdc6f3692439a62f5d421884ab2
Meng <kaomengkung@gmail.com>
2026-04-07 05:41:07 +0000

    gogo

---
+++ Datarecording/name_tester.py
@@ -0,0 +1,11 @@
+def main():
+    name = input("Please enter your name: ")
+    if name == "Meng":
+        print("Awesome name!")
+    else: 
+        print("Silly name!")
+
+if __name__ == "__main__":
+    main()
+
+
